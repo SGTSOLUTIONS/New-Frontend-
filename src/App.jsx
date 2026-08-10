ea044a4f7061e6f9e6874b947dd5dd3f073bd763
coimbatoresurvey62-blip <sgtsolutions24@gmail.com>
2026-08-10 14:37:09 +0530

    main
    i change api files

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
-<<<<<<< Updated upstream
 import Service from './pages/Service';
-=======
-import Login from './pages/Login';
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -15,14 +11,9 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
       </Route>
-<<<<<<< Updated upstream
       <Route path="/Service" element={<MainLayout />}>
         <Route index element={<Service />} />
       </Route>
-=======
-
-      <Route path="/login" element={<Login />} />
->>>>>>> Stashed changes
     </Routes>
   );
 }
